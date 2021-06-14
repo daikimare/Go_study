f1ed375d14a29ee190085be11b609889c5d13328
daikimare <b1840702@planet.kanazawa-it.ac.jp>
2021-06-15 04:08:08 +0900

    byte

---
+++ lesson3/byte.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+// 型
+// byte (unit8)型
+
+func main() {
+	byteA := []byte{72, 73}
+	fmt.Println(byteA)
+
+	// change string
+	fmt.Println(string(byteA))
+
+	// change byte from string
+	c := []byte("HI")
+	fmt.Println(c)
+
+	fmt.Println(string(c))
+}
