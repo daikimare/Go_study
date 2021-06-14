7aaf56b1de038a1f337bcf4c6ba4f4af0c3c166d
daikimare <b1840702@planet.kanazawa-it.ac.jp>
2021-06-15 03:52:54 +0900

    using variable

---
+++ lesson3/string.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var s string = "hogefuga"
+	fmt.Println(s)
+	fmt.Printf("%T\n",s)
+
+	var si string = "300"
+	fmt.Println(si)
+	fmt.Printf("%T\n", si)
+
+}
